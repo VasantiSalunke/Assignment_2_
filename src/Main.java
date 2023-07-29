eea38008aa8f48dd89d868dfc1ea65bdd43a8e3c
vasanti <vasanti.salunke27@gmail.com>
2023-07-29 21:41:49 +0530

    Assignment_2

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Stack nums = new Stack();
+        nums.push(10);
+        nums.push(20);
+
+        System.out.println(nums.peek());
+
+        nums.push(30);
+        nums.push(40);
+
+        System.out.println(nums.pop());
+
+        nums.push(50);
+        nums.push(60);
+        nums.push(70);
+
+
+        nums.show();
+    }
+}
